fcb4f104281ca491bea2eae9ff833b2fda28c4b7
vanessa abordo <jocogum10@gmail.com>
2019-09-26 21:39:51 +0800

    modified to list the found files

--- file_list.py
+++ file_list.py
@@ -9,6 +9,8 @@ dirname = input("Enter the directory: ")
 file_type = input("Enter the filetype: ")
 prompt = input("Name of file list: ")
 
+file_type_lower = file_type.lower()
+file_type_upper = file_type.upper()
 file_list = open("{0}.txt".format(prompt), 'w')
 visited = {}
 my_file = []
@@ -21,7 +23,19 @@ for (thisDir, subsHere, filesHere) in os.walk(dirname):
     else:
         visited[fixcase] = True
     for filename in filesHere:
-        if filename.endswith(file_type):
-            print('|--->', filename, file=file_list)
-
+        if filename.endswith(file_type_lower) or filename.endswith(file_type_upper):
+            try:
+                print('|--->', filename, file=file_list)
+                my_file.append(filename)
+            except UnicodeEncodeError:
+                pass
+print("###################################################################", file=file_list)
+print("\n", file=file_list)
+my_list = sorted(my_file)
+number_of_files = len(my_list)
+print("Number of files found:", file=file_list)
+print(number_of_files, file=file_list)
+for file in my_list:
+    print(file, file=file_list)
+    
 file_list.close()         
